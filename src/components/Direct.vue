374a8d752f6e4a0b9127536a8b4946f2bfc13b0b
Ji In Lee <loveted0225@naver.com>
2022-01-27 18:32:02 +0900

    11번가 클론코딩

---
+++ src/components/Direct.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="direct">
+    <div class="inner">
+      <ul>
+        <li 
+          v-for="item in directs"
+          :key="item.name">
+          <a :href="item.href">
+            <div>
+              <img 
+                :src="item.src" 
+                :alt="item.name"
+                width="90">
+              <div class="text">
+                {{item.name}}
+              </div>
+            </div>
+          </a>
+        </li>
+        <li>
+          <div 
+            class="open-more"
+            @click="onNav('LNB')">
+            <div class="icon"></div>
+            <div class="text">
+              더보기
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      directs: []
+    }
+  },
+  created(){
+    this.init()
+  },
+  methods: {
+    async init(){
+      this.directs = await this.$fetch({
+        requestName:'directs'
+      })
+    },
+    onNav(name){
+      this.$store.dispatch('navigations/onNav', name)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
